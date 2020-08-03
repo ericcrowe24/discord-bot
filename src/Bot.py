e76847cc96478cd8c2465c54d76a7bf44321f590
Eric Crowe <arktheoverlord@gmail.com>
2020-08-03 00:39:40 -0400

    Add an option for the bot to initialize the database

--- src/Bot.py
+++ src/Bot.py
@@ -9,9 +9,19 @@ def main(key):
 
 
 if __name__ == '__main__':
-    DataAccess.host = sys.argv[2]
-    DataAccess.user = sys.argv[3]
-    DataAccess.password = sys.argv[4]
-    DataAccess.database = sys.argv[5]
+    if sys.argv[2] == "-init" or sys.argv[2] == "-i":
+        DataAccess.host = sys.argv[3]
+        DataAccess.user = sys.argv[4]
+        DataAccess.password = sys.argv[5]
+        DataAccess.database = sys.argv[6]
+
+        db = DataAccess.Connection()
+        db.create_databases()
+        db.close()
+    else:
+        DataAccess.host = sys.argv[2]
+        DataAccess.user = sys.argv[3]
+        DataAccess.password = sys.argv[4]
+        DataAccess.database = sys.argv[5]
 
     main(sys.argv[1])
